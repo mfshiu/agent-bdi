43b9a9a1c5231573db83d843897c24fa0b9eefd7
你的名字 <avatar.xu@gmail.com>
2023-06-11 03:18:28 +0800

    trans

--- tests/guide/navi/__init__.py
+++ tests/guide/navi/__init__.py
@@ -1,13 +1,17 @@
+# import os, sys
+# sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))))
+
+import logging
+
+from src.holon import Helper
+from src.holon.HolonicAgent import HolonicAgent
 from navi.VisualInput import VisualInput
-from holon.HolonicAgent import HolonicAgent
 from navi.RouteFind import RouteFind
 from navi.walk.WalkGuide import WalkGuide
 
-class NaviSystem(HolonicAgent):
+class Navigator(HolonicAgent):
     def __init__(self, cfg):
         super().__init__(cfg)
         self.head_agents.append(VisualInput(cfg))
         self.body_agents.append(WalkGuide(cfg))
         self.body_agents.append(RouteFind(cfg))
-
-
