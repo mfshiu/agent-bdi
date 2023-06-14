3d25a71c32722f08afb67dc6084ebb422bd239f4
mfshiu <avatar.xu@gmail.com>
2023-06-15 01:48:24 +0800

    run_trains

--- tests/guide/hearing/trans.py
+++ tests/guide/hearing/trans.py
@@ -9,6 +9,8 @@ import whisper
 import torch
 
 from src.holon.HolonicAgent import HolonicAgent
+from src.holon import config
+import guide_config
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 # whisper.DecodingOptions(language="zh")
@@ -26,15 +28,28 @@ class Transcriptionist(HolonicAgent):
         super()._on_connect(client, userdata, flags, rc)
 
 
-    def _on_topic(self, topic, data):
-        if "hearing.voice" == topic:
-            # logging.debug(f"wave_path:{data}")
+    def _on_message(self, client, db, msg):
+        if "hearing.voice" == msg.topic:
+            data = msg.payload
             wave_path = dt.now().strftime("tests/_input/voice-%m%d-%H%M-%S.wav")
+            # logging.debug(f'data: {data}')
             with open(wave_path, "wb") as file:
-                file.write(data)
+                file.write(msg.payload)
             self.wave_queue.put(wave_path)
 
-        super()._on_topic(topic, data)
+        # super()._on_topic(topic, data)
+
+
+    # def _on_topic(self, topic, data):
+    #     if "hearing.voice" == topic:
+    #         # logging.debug(f"wave_path:{data}")
+    #         wave_path = dt.now().strftime("tests/_input/voice-%m%d-%H%M-%S.wav")
+    #         logging.debug(f'data: {data}')
+    #         with open(wave_path, "wb") as file:
+    #             file.write(data)
+    #         self.wave_queue.put(wave_path)
+
+    #     super()._on_topic(topic, data)
 
 
     def _run_begin(self):
@@ -69,5 +84,16 @@ class Transcriptionist(HolonicAgent):
 
 if __name__ == '__main__':
     logging.info('***** VoiceToText start *****')
-    a = Transcriptionist()
+
+    cfg = config()
+    # cfg.mqtt_address = guide_config.mqtt_address
+    # cfg.mqtt_port = guide_config.mqtt_port
+    # cfg.mqtt_keepalive = guide_config.mqtt_keepalive
+    # cfg.mqtt_username = guide_config.mqtt_username
+    # cfg.mqtt_password = guide_config.mqtt_password
+    # cfg.log_level = guide_config.log_level
+    # cfg.log_dir = guide_config.log_dir    
+    # os.environ["OPENAI_API_KEY"] = guide_config.openai_api_key
+
+    a = Transcriptionist(cfg)
     a.start()
