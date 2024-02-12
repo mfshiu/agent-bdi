b00f1d0415f5b98725b80d574a68925e01be4df3
你的名字 <avatar.xu@gmail.com>
2024-02-12 16:17:28 +0800

    ..

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding = "utf-8") as fh:
 
 setuptools.setup(
     name = "agent-bdi",
-    version = "2.3.0",
+    version = "2.3.2",
     author = "Ming Fang Shiu",
     author_email='avatar.xu@gmail.com',
     description='Agent BDI framework',
