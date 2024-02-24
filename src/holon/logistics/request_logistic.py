7e20fec76cdcd2ee3626cfa36e581824d527af23
你的名字 <avatar.xu@gmail.com>
2024-02-24 18:44:43 +0800

    request id

--- src/holon/logistics/request_logistic.py
+++ src/holon/logistics/request_logistic.py
@@ -13,9 +13,12 @@ SUBSCRIBE_HEADER = "@response"
 
 
 class RequestLogistic(BaseLogistic):
-    def __init__(self, agent:HolonicAgent, request_topic, response_handler=None, datatype="str"):
+    def __init__(self, agent:HolonicAgent, 
+                 request_topic:str, response_handler=None, 
+                 request_id="", datatype="str"):
         self.agent = agent
         self.request_topic = request_topic
+        self.request_id = request_id
         
         response_topic = f"{SUBSCRIBE_HEADER}.{request_topic}"
         self.agent.subscribe(response_topic, datatype, self.handle_response)        
@@ -30,13 +33,14 @@ class RequestLogistic(BaseLogistic):
         unpacked = self.unpack(payload)
         logger.debug(f"receiver: {unpacked['receiver']}, agent_id: {self.agent.agent_id}")
         if unpacked["receiver"] == self.agent.agent_id:
-            self.agent._on_message(responsed_topic, unpacked["content"])
+            if unpacked["request_id"] == self.request_id:
+                self.agent._on_message(responsed_topic, unpacked["content"])
 
 
     def pack(self, topic:str, payload):
         if topic == self.request_topic:
-            request_id = str(uuid.uuid4())
-            packed = self._payload_wrapper.wrap_for_request(payload, request_id)
+            # request_id = str(uuid.uuid4())
+            packed = self._payload_wrapper.wrap_for_request(payload, self.request_id)
             # logger.debug(f"packed: {packed}")
             return f"{PUBLISH_HEADER}.{topic}", packed
         else:
